625fbb6f49862609953768acb7215f6dd510ccec
TCODeva <31217479+TCODeva@users.noreply.github.com>
2018-06-21 15:29:40 +0800

    Update Power.cpp

--- Power.cpp
+++ Power.cpp
@@ -2,15 +2,15 @@
 double pow(double a, int n) {
        if(n == 0) return 1;
        if(n == 1) return a;
-       double t = pow(a, n/2);
-       return t * t * pow(a, n%2);
+       double t = pow(a, n / 2);
+       return t * t * pow(a, n % 2);
 }
 
 //non-recursive
 double pow(double a, int n) {
        double ret = 1;
        while(n) {
-           if(n%2 == 1) ret *= a;
+           if(n % 2 == 1) ret *= a;
            a *= a; n /= 2;
        }
        return ret; 
