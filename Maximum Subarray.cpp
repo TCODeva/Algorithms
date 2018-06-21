e4ee75995b7f96b2add6e4b537c6cc0ff13163cd
TCODeva <31217479+TCODeva@users.noreply.github.com>
2018-06-21 21:15:48 +0800

    Update Maximum Subarray.cpp

--- Maximum Subarray.cpp
+++ Maximum Subarray.cpp
@@ -28,3 +28,21 @@ public:
         return totalSum;
     }
 };
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = nums[0];
+        int dp[nums.size()];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (dp[i-1] > 0) 
+                dp[i] = dp[i-1] + nums[i];
+            else 
+                dp[i] = nums[i];
+            if (dp[i] > sum) 
+                sum = dp[i];
+        }
+        return sum;
+    }
+};
