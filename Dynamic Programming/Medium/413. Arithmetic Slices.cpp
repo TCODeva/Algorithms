4e5db23bfab2294af6ac1d2bbd07e3edc9a31bfe
TCODeva <31217479+TCODeva@users.noreply.github.com>
2018-06-23 07:37:54 +0800

    Update 413. Arithmetic Slices.cpp

--- Dynamic Programming/Medium/413. Arithmetic Slices.cpp
+++ Dynamic Programming/Medium/413. Arithmetic Slices.cpp
@@ -40,3 +40,18 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        int curr = 0, sum = 0;
+        for (int i = 2; i < A.size(); i++)
+        if (A[i]-A[i-1] == A[i-1]-A[i-2]) {
+            curr += 1;
+            sum += curr;
+        } else {
+            curr = 0;
+        }
+    return sum;
+    }
+};
