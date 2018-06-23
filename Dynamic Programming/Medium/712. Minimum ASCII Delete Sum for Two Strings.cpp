585deefbddca2ba339880c98776c72baf9c622df
TCODeva <31217479+TCODeva@users.noreply.github.com>
2018-06-23 11:00:36 +0800

    Create 712. Minimum ASCII Delete Sum for Two Strings.cpp

---
+++ Dynamic Programming/Medium/712. Minimum ASCII Delete Sum for Two Strings.cpp
@@ -0,0 +1,47 @@
+/* LeetCode 712. Minimum ASCII Delete Sum for Two Strings
+ * Given two strings s1, s2, find the lowest ASCII sum of deleted characters to make two strings equal.
+ * 
+ * Example 1:
+ * Input: s1 = "sea", s2 = "eat"
+ * Output: 231
+ * Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum.
+ * Deleting "t" from "eat" adds 116 to the sum.
+ * At the end, both strings are equal, and 115 + 116 = 231 is the minimum sum possible to achieve this.
+ * Example 2:
+ * Input: s1 = "delete", s2 = "leet"
+ * Output: 403
+ * Explanation: Deleting "dee" from "delete" to turn the string into "let",
+ * adds 100[d]+101[e]+101[e] to the sum.  Deleting "e" from "leet" adds 101[e] to the sum.
+ * At the end, both strings are equal to "let", and the answer is 100+101+101+101 = 403.
+ * If instead we turned both strings into "lee" or "eet", we would get answers of 433 or 417, which are higher.
+ * Note:
+ * 
+ * 0 < s1.length, s2.length <= 1000.
+ * All elements of each string will have an ASCII value in [97, 122].
+ */
+ 
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        int dp[n+1][m+1];
+        for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        for(int j = 0; j <= m; j++) dp[0][j] = 0;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if (s1[i-1] == s2[j-1]) 
+                    dp[i][j] = dp[i-1][j-1] + int(s1[i-1]);
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return count(s1) + count(s2) - 2 * dp[n][m];
+    }
+    
+    int count(string s) {
+        int n = 0;
+        for (int i = 0; i < s.size(); i++)
+            n += int(s[i]);
+        return n;
+    }
+};
