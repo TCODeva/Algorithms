ad2c510dc4e1c2012807a045f235a23c71b68727
TCODeva <31217479+TCODeva@users.noreply.github.com>
2018-06-23 10:58:51 +0800

    Update Longest Common Subsequence.cpp

--- Useful Functions/Longest Common Subsequence.cpp
+++ Useful Functions/Longest Common Subsequence.cpp
@@ -5,7 +5,7 @@ int longestCommonSubsequence(string s1, string s2) {
     for(int j = 0; j <= m; j++) dp[0][j] = 0;
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= m; j++) {
-            if (s1[i] == s2[j])
+            if (s1[i-1] == s2[j-1])
                 dp[i][j] = dp[i-1][j-1] + 1;    
             else
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
