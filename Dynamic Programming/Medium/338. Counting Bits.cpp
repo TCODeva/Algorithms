144675bb63581f8ffd035312ba6cb04b58558c1b
TCODeva <31217479+TCODeva@users.noreply.github.com>
2018-06-23 09:28:23 +0800

    Update 338. Counting Bits.cpp

--- Dynamic Programming/Medium/338. Counting Bits.cpp
+++ Dynamic Programming/Medium/338. Counting Bits.cpp
@@ -9,7 +9,8 @@
  * It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
  * Space complexity should be O(n).
  * Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
- 
+ */
+
 class Solution {
 public:
     vector<int> countBits(int num) {
