c229351992ad390b44bd526292f1e058ad2bce40
TCODeva <31217479+TCODeva@users.noreply.github.com>
2018-07-02 20:42:04 +0800

    Create 343. Integer Break.cpp

---
+++ Dynamic Programming/Medium/343. Integer Break.cpp
@@ -0,0 +1,21 @@
+/* LeetCode 343. Integer Break
+ * Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. Return the maximum product you can get.
+ * 
+ * For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 + 4).
+ * 
+ * Note: You may assume that n is not less than 2 and not larger than 58.
+ */
+ 
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n <= 3) return n-1;
+        int i = 1;
+        while (n > 3) {
+            if (n == 4) return i * 4;
+            i *= 3;
+            n -= 3;
+        }
+        return i * n;
+    }
+};
