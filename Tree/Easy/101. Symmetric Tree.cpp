daf40ca4423ea7b8e626b2a7b8022fea3469839d
TCODeva <31217479+TCODeva@users.noreply.github.com>
2018-08-27 11:45:02 -0400

    Update 101. Symmetric Tree.cpp

--- Tree/Easy/101. Symmetric Tree.cpp
+++ Tree/Easy/101. Symmetric Tree.cpp
@@ -39,3 +39,26 @@ private:
         return p->val == q->val && isMirror(p->left,q->right) && isMirror(p->right,q->left);
     }
 };
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* t1 = q.front();
+            q.pop();
+            TreeNode* t2 = q.front();
+            q.pop();
+            if (!t1 && !t2) continue;
+            if (!t1 || !t2) return false;
+            if (t1->val != t2->val) return false;
+            q.push(t1->left);
+            q.push(t2->right);
+            q.push(t1->right);
+            q.push(t2->left);
+        }
+        return true;
+    }
+};
